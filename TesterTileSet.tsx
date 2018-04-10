b4e3f2efe09fac79f0a566876f4fa3ef604c514b
Jaime Bews <bews.a.james@gmail.com>
2018-04-10 18:29:46 -0400

    Added my map files

---
+++ TesterTileSet.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TesterTileSet" tilewidth="40" tileheight="40" tilecount="2" columns="0">
+ <tile id="2">
+  <image width="20" height="20" source="../../../Desktop/repeatWorldtile.png"/>
+ </tile>
+ <tile id="3">
+  <image width="40" height="40" source="../../../Desktop/GreenbushLOL.png"/>
+ </tile>
+</tileset>
