73677ee76f6d55d4204fe4efab6971275c8a2a56
MacDonald Maunder <macdonald.maunder@georgebrown.ca>
2018-04-15 13:13:39 -0400

    Added Main Menu
    added the main menu with:
    images and buttons
    
    still need to set as activity the game opens to

---
+++ SideScroller/app/src/main/java/com/jaime/sidescroller/MenuActivity.java
@@ -0,0 +1,48 @@
+package com.jaime.sidescroller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button playButtonVariable = (Button) findViewById(R.id.b_level1);
+        PlayButtonListener playListenerVariable = new PlayButtonListener();
+        playButtonVariable.setOnClickListener(playListenerVariable);
+
+        Button ButtonVariable = (Button) findViewById(R.id.b_level2);
+        ButtonListener ListenerVariable = new ButtonListener();
+        ButtonVariable.setOnClickListener(ListenerVariable);
+    }
+
+    private class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            startPlay2();
+        }
+    }
+
+    private void startPlay1() {
+        Intent intentInstance = new Intent(this, MainActivity.class);
+        startActivity(intentInstance);
+    }
+
+    private void startPlay2() {
+        Intent intentInstance = new Intent(this, MainActivity.class);
+        startActivity(intentInstance);
+    }
+
+    private class PlayButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            startPlay1();
+        }
+    }
+}
