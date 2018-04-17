3a88adfe38b730362bf379c38bae72e6466dcae0
Dennis Nguyen <nichidg@hotmail.com>
2018-04-17 08:23:16 -0400

    added documentation

---
+++ M.M_NewTileSet.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="M.M_NewTileSet" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="brick">
+  <image width="64" height="64" source="brick.png"/>
+ </tile>
+ <tile id="1" type="dirt_top">
+  <image width="64" height="64" source="dirt.png"/>
+ </tile>
+ <tile id="2" type="dirt_bot">
+  <image width="64" height="64" source="dirt_grass.png"/>
+ </tile>
+ <tile id="3" type="end_Block">
+  <image width="64" height="64" source="end_point.png"/>
+ </tile>
+ <tile id="4" type="Enemy">
+  <image width="64" height="64" source="golem_still.png"/>
+ </tile>
+</tileset>
